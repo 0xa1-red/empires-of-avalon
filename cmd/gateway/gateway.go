50b27be56d162c3a889cbbff04e6c29c618f5e36
Alfred Dobradi <alfreddobradi@gmail.com>
2023-07-01 21:25:48 +0100

    feat: Refactor handshake and add sequences

--- cmd/gateway/gateway.go
+++ cmd/gateway/gateway.go
@@ -1,26 +1,45 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 
 	"github.com/0xa1-red/empires-of-avalon/config"
 	"github.com/0xa1-red/empires-of-avalon/logging"
+	"github.com/0xa1-red/empires-of-avalon/pkg/service/blueprints"
+	"github.com/0xa1-red/empires-of-avalon/protobuf"
 	intnats "github.com/0xa1-red/empires-of-avalon/transport/nats"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 	"github.com/nats-io/nats.go"
 	"golang.org/x/exp/slog"
+	"google.golang.org/protobuf/proto"
 )
 
-var wsupgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
+var (
+	wsupgrader = websocket.Upgrader{ // nolint:exhaustruct
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	// Time allowed to write a message to the peer.
+	// writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	// pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	// pingPeriod = (pongWait * 9) / 10
+)
 
 var hub = map[uuid.UUID]*Client{}
 
@@ -30,9 +49,64 @@ type Message struct {
 }
 
 type Client struct {
-	Connection *websocket.Conn
-	UserID     uuid.UUID
-	Outgoing   chan Message
+	Connection  *Connection
+	UserID      uuid.UUID
+	InventoryID uuid.UUID
+	Outgoing    chan Message
+}
+
+type Connection struct {
+	*websocket.Conn
+}
+
+func (conn *Connection) handshake(msg []byte) (*Client, error) {
+	handshake := make(map[string]string)
+	if err := json.Unmarshal(msg, &handshake); err != nil {
+		slog.Error("failed to unmarshal handshake message", err, "message", msg)
+		return nil, fmt.Errorf("failed to unmarshal handshake message: %w", err)
+	}
+
+	id, err := uuid.Parse(handshake["UserID"])
+	if err != nil {
+		slog.Error("failed to parse user ID", err, "user_id", handshake["UserID"])
+		return nil, fmt.Errorf("failed to parse user ID: %w", err)
+	}
+
+	if err := conn.SetReadDeadline(time.Time{}); err != nil {
+		slog.Error("failed to set read deadline", err)
+	}
+
+	inventoryID := blueprints.GetInventoryID(id)
+
+	return &Client{
+		Connection:  conn,
+		UserID:      id,
+		InventoryID: inventoryID,
+		Outgoing:    make(chan Message),
+	}, nil
+}
+
+func (conn *Connection) writeMessage(msg interface{}) error {
+	var err error
+
+	retries := 3
+
+	for {
+		err = conn.WriteJSON(msg)
+		if err != nil {
+			retries--
+		}
+
+		if err == nil || retries == 0 {
+			break
+		}
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 var configPath string
@@ -42,11 +116,20 @@ func main() {
 	flag.Parse()
 
 	config.Setup(configPath)
-	logging.Setup()
+
+	if err := logging.Setup(); err != nil {
+		os.Exit(1)
+	}
+
 	r := gin.Default()
 
-	nc := intnats.GetConnection()
-	nc.Subscribe("status.*", func(m *nats.Msg) {
+	nc, err := intnats.GetConnection()
+	if err != nil {
+		slog.Error("failed to get NATS connection", err)
+		os.Exit(1) // nolint
+	}
+
+	_, subErr := nc.Subscribe("status.*", func(m *nats.Msg) {
 		userID := strings.TrimPrefix(m.Subject, "status.")
 		id, err := uuid.Parse(userID)
 		if err != nil {
@@ -54,13 +137,34 @@ func main() {
 			return
 		}
 
+		slog.Debug("received message", "subject", m.Subject)
+
+		i := protobuf.InventoryStatusUpdate{} // nolint:exhaustruct
+		if err := proto.Unmarshal(m.Data, &i); err != nil {
+			slog.Error("failed to unmarshal protobuf message", err)
+			return
+		}
+
+		b := bytes.NewBuffer([]byte(""))
+		encoder := json.NewEncoder(b)
+		if err := encoder.Encode(&i); err != nil {
+			slog.Error("failed to encode json message", err)
+			return
+		}
+
 		if client, ok := hub[id]; ok {
 			client.Outgoing <- Message{
 				Timestamp: time.Now(),
-				Message:   string(m.Data),
+				Message:   b.String(),
 			}
 		}
 	})
+
+	if subErr != nil {
+		slog.Error("failed to subscribe to status subject", err)
+		os.Exit(1)
+	}
+
 	defer nc.Close()
 
 	r.GET("/ws", func(c *gin.Context) {
@@ -68,60 +172,79 @@ func main() {
 	})
 
 	fmt.Println("http://localhost:3000")
-	r.Run("localhost:3000")
+
+	if err := r.Run("localhost:3000"); err != nil {
+		slog.Error("http server returned error", err)
+	}
 }
 
 func wshandler(w http.ResponseWriter, r *http.Request) {
-	auth := r.Header.Get("Authorization")
-	if auth == "" {
-		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
-		return
-	}
-
-	id, err := uuid.Parse(auth)
+	wsConn, err := wsupgrader.Upgrade(w, r, nil)
 	if err != nil {
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		slog.Error("failed to set websocket upgrade", err)
 		return
 	}
 
-	conn, err := wsupgrader.Upgrade(w, r, nil)
+	conn := &Connection{
+		Conn: wsConn,
+	}
+
+	if err := conn.SetReadDeadline(time.Now().Add(30 * time.Second)); err != nil {
+		slog.Error("failed to set read deadline", err)
+		conn.Close()
+
+		return
+	}
+
+	_, msg, err := conn.ReadMessage()
 	if err != nil {
-		fmt.Printf("Failed to set websocket upgrade: %+v", err)
+		slog.Error("failed to read message", err)
+		conn.Close()
+
 		return
 	}
 
-	hub[id] = &Client{
-		Connection: conn,
-		UserID:     id,
-		Outgoing:   make(chan Message),
+	client, err := conn.handshake(msg)
+	if err != nil {
+		slog.Error("failed to validate handshake", err)
+		conn.Close()
+
+		return
 	}
-	slog.Info("client connected", "user_id", id, "connection", conn.RemoteAddr())
+
+	hub[client.InventoryID] = client
+	slog.Info("client connected", "user_id", client.UserID, "inventory_id", client.InventoryID, "connection", conn.RemoteAddr())
+
 	defer func() {
-		delete(hub, id)
+		delete(hub, client.UserID)
 	}()
 
-	go func() {
-		for msg := range hub[id].Outgoing {
-			if err := conn.WriteJSON(msg); err != nil {
-				slog.Error("failed to write message", err, "user_id", id, "connection", conn.RemoteAddr())
-			}
-		}
-	}()
+	go watchOutgoing(hub, client)
 
 	for {
 		t, _, err := conn.ReadMessage()
-		if t != -1 {
+		if t != -1 || err == nil {
 			continue
 		}
-		if err != nil {
-			if websocket.IsCloseError(err, websocket.CloseNormalClosure) {
-				slog.Info("client connected", "user_id", id, "connection", conn.RemoteAddr())
-				conn.Close()
-				return
-			}
-			slog.Error("failed to read message", err, "user_id", id, "connection", conn.RemoteAddr())
+
+		if websocket.IsCloseError(err, websocket.CloseNormalClosure) {
+			slog.Info("client disconnected", "user_id", client.UserID, "connection", conn.RemoteAddr())
 			conn.Close()
+
 			return
+		}
+
+		slog.Error("failed to read message", err, "user_id", client.UserID, "connection", conn.RemoteAddr())
+		conn.Close()
+
+		return
+	}
+}
+
+func watchOutgoing(hub map[uuid.UUID]*Client, client *Client) {
+	for msg := range hub[client.InventoryID].Outgoing {
+		if err := client.Connection.writeMessage(msg); err != nil {
+			slog.Error("failed to write message", err, "user_id", client.UserID, "connection", client.Connection.RemoteAddr())
 		}
 	}
 }

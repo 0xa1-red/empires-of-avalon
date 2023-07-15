76573342a69183de878b9a9974aa37626d7c239e
Alfred Dobradi <alfreddobradi@gmail.com>
2023-07-15 22:54:31 +0100

    fix(EOA-33): Change blob to bytea

--- init/create.sql
+++ init/create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE snapshots (
     kind varchar(255) not null,
     identity uuid not null,
-    data blob not null,
+    data bytea not null,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
